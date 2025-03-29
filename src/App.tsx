36960f7fc28bdd9eb0e10c000e1ae03149f14f7d
ml5885 <limichael353@gmail.com>
2025-03-29 00:37:12 -0400

    included inspiration credits

--- src/App.tsx
+++ src/App.tsx
@@ -6,6 +6,18 @@ const App: React.FC = () => {
 	return (
 		<div className="App">
 			<FolderUI />
+			{/* full credit for this idea goes to https://x.com/samdape/status/1777986265993875950 */}
+			<div className="credit">
+				<p>
+					<a
+						href="https://x.com/samdape/status/1777986265993875950"
+						target="_blank"
+						rel="noopener noreferrer"
+					>
+						Inspired by @samdape
+					</a>
+				</p>
+			</div>
 		</div>
 	);
 };
